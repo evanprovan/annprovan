be8ee4eee010db214ab6343547755e241e2e9132
Evan Provan <evanprovan@Evans-MacBook-Pro.local>
2023-05-23 19:15:58 -0400

    added Async server component typescript workaround from Next Docs

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -2,6 +2,9 @@ import { getHeaderItems } from '@/sanity/sanity-utils'
 import Navbar from './Navbar'
 import { HeaderItems } from '@/types/HeaderItems'
 
+{
+    /* @ts-expect-error Async Server Component */
+}
 export default async function TheHeader() {
     const { works, category }: HeaderItems = await getHeaderItems()
     return (
