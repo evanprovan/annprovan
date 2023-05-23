6c14c272ec9ceb1eadc22cb850b5f2108abc3dc1
Evan Provan <evanprovan@Evans-MacBook-Pro.local>
2023-05-23 18:49:10 -0400

    Renamed Header to TheHeader

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import '../globals.css'
 import { Inter } from 'next/font/google'
-import Header from '@/components/Header'
+import TheHeader from '@/components/TheHeader'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -8,16 +8,12 @@ export const metadata = {
     description: 'American Artist',
 }
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body className={inter.className}>
                 <main>
-                    <Header />
+                    <TheHeader />
 
                     {children}
                 </main>
--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -2,7 +2,7 @@ import { getHeaderItems } from '@/sanity/sanity-utils'
 import Navbar from './Navbar'
 import { HeaderItems } from '@/types/HeaderItems'
 
-export default async function Header(): Promise<JSX.Element> {
+export default async function TheHeader(): Promise<JSX.Element> {
     const { works, category }: HeaderItems = await getHeaderItems()
     return (
         <header>
